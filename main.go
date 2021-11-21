accbd4eb98e368770d5cf03e5f4c88738fa13c8a
nms-7 <nixon.ubud@gmail.com>
2021-11-21 18:28:12 -0500

    first operational handler - query pools (scratch)

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/shurcooL/graphql"
 	"net/http"
 )
 
-type address string
+// type address string
 
 type Asset struct {
-	id address `json:"id"`
+	Id string `json:"id"`
 }
 
 type PoolQ struct {
@@ -26,21 +25,18 @@ var client *graphql.Client
 
 func init() {
 	client = graphql.NewClient("https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-alt", nil)
-	fmt.Println(client)
 }
 
 func main() {
-	/*
-	   server := http.Server{
-	       Addr: "127.0.0.1:8080",
-	   }
-	   http.HandleFunc("/pools/", getPools)
-	   server.ListenAndServe()
-	*/
-	queryPools()
+    server := http.Server{
+       Addr: "127.0.0.1:8080",
+    }
+    http.HandleFunc("/pools/", getPools)
+    server.ListenAndServe()
 }
 
 func getPools(w http.ResponseWriter, r *http.Request) {
+    /* decode */
 	decoder := json.NewDecoder(r.Body)
 	var asset Asset
 	err := decoder.Decode(&asset)
@@ -48,20 +44,26 @@ func getPools(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+    poolQ, err := queryPools(asset.Id)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError) // replace with other error?
+        return
+    }
+    encoder := json.NewEncoder(w)
+    err = encoder.Encode(poolQ)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    return
 }
 
 // graphql logic + request(s)
-func queryPools() {
-	var poolQ PoolQ
+func queryPools(assetId string) (poolQ PoolQ, err error) {
 	variables := map[string]interface{}{
-		// temp constant
-		"assetId": graphql.ID("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"),
+		"assetId": graphql.ID(assetId),
 	}
-	fmt.Println(client)
-	err := client.Query(context.Background(), &poolQ, variables)
-	if err != nil {
-		fmt.Println("error mofo:", err)
-	}
-	fmt.Println(poolQ)
-	// encode and write response
+	err = client.Query(context.Background(), &poolQ, variables)
+    return
 }
