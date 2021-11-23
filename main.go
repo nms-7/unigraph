c53bb965f48a634979ce13fc7e08513eab4110ad
nms-7 <nixon.ubud@gmail.com>
2021-11-23 17:31:01 -0500

    code cleanup

--- main.go
+++ main.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"context"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 	"github.com/shurcooL/graphql"
 	"net/http"
 )
-
-// type address string
 
 type AssetPoolsRequest struct {
 	Id string `json:"id"`
 }
 
 type AssetPoolsResponse struct {
-    Pools []Pool `json:"pools"`
+	Pools []Pool `json:"pools"`
 }
 
-
 type Pool struct {
-    Id graphql.String `json:"id"`
-    Token0 Token `json:"token0"`
-    Token1 Token `json:"token1"`
+	Id     graphql.String `json:"id"`
+	Token0 Token          `json:"token0"`
+	Token1 Token          `json:"token1"`
 }
 
 type Token struct {
-    Id graphql.String `json:"id"`
-    Symbol graphql.String `json:"symbol"`
+	Id     graphql.String `json:"id"`
+	Symbol graphql.String `json:"symbol"`
 }
 
 var client *graphql.Client
@@ -36,59 +33,52 @@ func init() {
 }
 
 func main() {
-    router := gin.Default()
-    router.GET("/pools", getPools)
-    router.Run("localhost:8080")
+	router := gin.Default()
+	router.GET("/pools", getPools)
+	router.Run("localhost:8080")
 }
 
 func getPools(c *gin.Context) {
-    var request AssetPoolsRequest
-    if err := c.BindJSON(&request); err != nil {
-        c.IndentedJSON(http.StatusInternalServerError, err.Error())
-        return
-    }
+	var request AssetPoolsRequest
+	if err := c.BindJSON(&request); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err.Error())
+		return
+	}
 
-    response, err := queryPools(request.Id)
-    if err != nil {
-        c.IndentedJSON(http.StatusInternalServerError, err.Error())
-        // replace with other error?
-        return
-    }
+	response, err := queryPools(request.Id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadGateway, err.Error())
+		return
+	}
 
-    c.IndentedJSON(http.StatusOK, response)
+	c.IndentedJSON(http.StatusOK, response)
 }
 
-// graphql logic + request(s)
 func queryPools(assetId string) (pools AssetPoolsResponse, err error) {
-    var poolQ0 struct {
-        Pools []Pool `graphql:"pools(where: { token0: $assetId } orderyBy: volumeUSD orderDirection: desc)"`
-    }
+	var poolQ0 struct {
+		Pools []Pool `graphql:"pools(where: { token0: $assetId })"`
+	}
 
-    var poolQ1 struct {
-        Pools []Pool `graphql:"pools(where: { token1: $assetId } orderyBy: volumeUSD orderDirection: desc)"`
-    }
+	var poolQ1 struct {
+		Pools []Pool `graphql:"pools(where: { token1: $assetId })"`
+	}
 	variables := map[string]interface{}{
 		"assetId": graphql.ID(assetId),
 	}
-    errs := make(chan error, 2)
-    go queryGraph(&poolQ0, variables, errs)
-    go queryGraph(&poolQ1, variables, errs)
-    for i := 0; i < 2; i++ {
-        err = <-errs
-        if err != nil {
-            return
-        }
-    }
-    // chan for appending?
-    pools.Pools = append(poolQ0.Pools, poolQ1.Pools...)
-    // sort?
-    // wait for both queries to complete
-    // benchmark this methodology for proof of implementation
-    return
+	errs := make(chan error, 2)
+	go queryGraph(&poolQ0, variables, errs)
+	go queryGraph(&poolQ1, variables, errs)
+	for i := 0; i < 2; i++ {
+		err = <-errs
+		if err != nil {
+			return
+		}
+	}
+	pools.Pools = append(poolQ0.Pools, poolQ1.Pools...)
+	return
 }
 
-// add chan to pass errors?
 func queryGraph(query interface{}, variables map[string]interface{}, errs chan error) {
-    err := client.Query(context.Background(), query, variables)
-    errs <- err
+	err := client.Query(context.Background(), query, variables)
+	errs <- err
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func BenchmarkQueryPools(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        queryPools("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48")
-    }
+	for i := 0; i < b.N; i++ {
+		// USDC
+		queryPools("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48")
+	}
 }
